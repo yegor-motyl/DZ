5e7232af67af61314676c636d31dce22a81156e1
yegor-motyl <98752487+yegor-motyl@users.noreply.github.com>
2022-02-02 19:04:38 +0700

    Add files via upload

---
+++ 9.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+#define MAX 1000 
+
+int n;
+std::vector<std::vector<int>> g[MAX];
+bool used[MAX];
+vector<int> comp;
+ 
+void dfs (int v){
+	used[v]=true;
+	comp.push_back (v);
+	for (size_t i=0; i<g[v].size(); ++i) {
+		int to = g[v][i];
+		if (!used[to])
+			dfs (to);
+	}
+}
+ 
+void find_comps(){
+	for (int i=0; i<n; ++i)
+		used[i] = false;
+	for (int i=0; i<n; ++i)
+		if (! used[i]) {
+			comp.clear();
+			dfs (i);
+			std::cout << comp.size();
+		}
+}
+
+int main(){
+    int v,e;
+    std::cin >> v >> e;
+    for (int i=0; i<e; i+=2){
+        int temp1,temp2;
+        std::cin >> temp1 >> temp2 << '\n';
+        g[i]=temp1;
+        g[i+1]=temp2;
+    }
+    find_comps();
+    return 0;
+}
